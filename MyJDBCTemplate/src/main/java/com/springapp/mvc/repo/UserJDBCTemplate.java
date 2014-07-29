7a0c6d7ba4018d975e424e06573e35e18217b764
KhrystynaMakar <tinochka0@gmail.com>
2014-07-29 12:21:51 +0300

    Add namedJDBCTemplate

--- MyJDBCTemplate/src/main/java/com/springapp/mvc/repo/UserJDBCTemplate.java
+++ MyJDBCTemplate/src/main/java/com/springapp/mvc/repo/UserJDBCTemplate.java
@@ -1,56 +1,69 @@
 package com.springapp.mvc.repo;
 
 import com.springapp.mvc.dao.UserDAO;
-import com.springapp.mvc.entity.Role;
 import com.springapp.mvc.entity.User;
 
 import javax.sql.DataSource;
-import java.util.List;
-import java.util.Locale;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import com.springapp.mvc.service.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 public class UserJDBCTemplate implements UserDAO {
     private static final String BUNDLE_NAME = "userQueries_en";
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplateObject;
     private ResourceBundle userRB = ResourceBundle.getBundle(BUNDLE_NAME, Locale.ENGLISH);
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public void create(String name) {
-        jdbcTemplateObject.update(userRB.getString("create"), name);
+//        jdbcTemplateObject.update(userRB.getString("create"), name);
+        Map namedParameters = new HashMap();
+        namedParameters.put("name", name);
+        namedParameterJdbcTemplate.update(userRB.getString("create"), namedParameters);
         System.out.println("Created Record Name = " + name);
     }
 
     @Override
     public void updateName(Integer id, String name) {
-        jdbcTemplateObject.update(userRB.getString("update"), name, id);
+//        jdbcTemplateObject.update(userRB.getString("update"), name, id);
+        Map namedParameters = new HashMap();
+        namedParameters.put("name", name);
+        namedParameters.put("id", id);
+        namedParameterJdbcTemplate.update(userRB.getString("update"), namedParameters);
         System.out.println("Updated Record with ID = " + id );
     }
 
     @Override
     public User getUser(Integer id) {
-        return jdbcTemplateObject.queryForObject(userRB.getString("findOne"), new Object[]{id}, new UserMapper());
+//        return jdbcTemplateObject.queryForObject(userRB.getString("findOne"), new Object[]{id}, new UserMapper());
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        return namedParameterJdbcTemplate.queryForObject(userRB.getString("findOne"), namedParameters, new UserMapper());
     }
 
     @Override
     public List<User> getUsers() {
+//        return jdbcTemplateObject.query(userRB.getString("findAll"), new UserMapper());
         return jdbcTemplateObject.query(userRB.getString("findAll"), new UserMapper());
     }
 
     @Override
     public void deleteUser(Integer id) {
-        jdbcTemplateObject.update(userRB.getString("delete"), id);
+//        jdbcTemplateObject.update(userRB.getString("delete"), id);
+        Map namedParameters = new HashMap();
+        namedParameters.put("id", id);
+        namedParameterJdbcTemplate.update(userRB.getString("delete"), namedParameters);
         System.out.println("Deleted Record with ID = " + id );
     }
 }
